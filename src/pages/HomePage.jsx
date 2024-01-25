e23b45556702b8c834f318610f1322284ec0c66e
Catherine Fournier <fourniercath@hotmail.com>
2024-01-25 16:57:36 +0100

    created routes in app.jsx, created navbar, hp, and new trip page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <>
+        <h1>Welcome to TripPlanner !</h1>
+        <p>Say goodbye to the hassle of planning holidays with friends! Are you tired of spending too much time organizing every detail? Let us handle the boring part, so you can sit back, relax, and enjoy your vacation !</p>
+        </>
+    )}
+
+export default HomePage
---
+++ src/pages/UpdateTripPage.jsx
@@ -0,0 +1,6 @@
+const UpdateTripPage = () => {
+    return (
+        <h1>Update trip page</h1>
+    )}
+
+export default UpdateTripPage
