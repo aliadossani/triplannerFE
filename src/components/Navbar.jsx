35152e62bb332087c0388cd707fb959bcac5645a
Emiliano Foti <foti.emiliano@gmail.com>
2024-01-25 17:52:37 +0100

    uncommented navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,40 @@
-import { useContext } from 'react'
-import { NavLink } from 'react-router-dom'
-import appLogo from '../assets/appLogo.png'
-import styles from '../styles/Navbar.module.css'
-// import { AuthContext } from '../contexts/AuthContext'
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import appLogo from "../assets/appLogo.png";
+import styles from "../styles/Navbar.module.css";
+import { AuthContext } from "../contexts/AuthContext";
 
 const Navbar = () => {
-  // const { isAuthenticated, logout } = useContext(AuthContext)
+  const { isAuthenticated, logout } = useContext(AuthContext);
 
   return (
     <nav className={styles.navbar}>
-      <NavLink to='/'> <img src={appLogo}/></NavLink>
-      <NavLink to='trips/new'><p>New Trip</p></NavLink>
-        {/* {isAuthenticated ? (
-          <>
-            <li>
-              <NavLink to='/trips/new'>New Trip</NavLink>
-            </li>
-            <li>
-              <button type='button' onClick={logout}>
-                Logout
-              </button>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <NavLink to='/signup'>Signup</NavLink>
-            </li>
-            <li>
-              <NavLink to='/login'>Login</NavLink>
-            </li>
-          </>
-        )} */}
-  
+      <NavLink to="/">
+        <img src={appLogo} />
+      </NavLink>
+      {isAuthenticated ? (
+        <>
+          <li>
+            <NavLink to="/trips/new">New Trip</NavLink>
+          </li>
+          <li>
+            <button type="button" onClick={logout}>
+              Logout
+            </button>
+          </li>
+        </>
+      ) : (
+        <>
+          <li>
+            <NavLink to="/signup">Signup</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+        </>
+      )}
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
