a5889dacd562fa5a6cd46a2e837303f51fb71fbc
Emiliano Foti <foti.emiliano@gmail.com>
2024-01-26 17:01:27 +0100

    added protected routes and conditional rendering of home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,20 @@ import { NavLink } from "react-router-dom";
 import appLogo from "../assets/appLogo.png";
 import styles from "../styles/Navbar.module.css";
 import { AuthContext } from "../contexts/AuthContext";
-import AboutUsPage from "../pages/AboutUsPage";
 
 const Navbar = () => {
   const { isAuthenticated, logout } = useContext(AuthContext);
 
   return (
     <nav className={styles.navbar}>
-      <NavLink to="/">
-        <img src={appLogo} />
-      </NavLink>
-      <NavLink to="/aboutUs">
-        <button type="button">AboutUs</button>
-      </NavLink>
       {isAuthenticated ? (
         <>
+          <NavLink to="/trips">
+            <img src={appLogo} />
+          </NavLink>
+          <NavLink to="/aboutUs">
+            <button type="button">AboutUs</button>
+          </NavLink>
           <NavLink to="/trips/new">
             <button type="button">New Trip</button>
           </NavLink>
@@ -27,6 +26,12 @@ const Navbar = () => {
         </>
       ) : (
         <>
+          <NavLink to="/">
+            <img src={appLogo} />
+          </NavLink>
+          <NavLink to="/aboutUs">
+            <button type="button">AboutUs</button>
+          </NavLink>
           <NavLink to="/signup">
             <button type="button">Signup</button>
           </NavLink>
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,14 +1,29 @@
 import { NavLink } from "react-router-dom";
 import appLogo from "../assets/appLogo.png";
-import { Center } from "@mantine/core";
 import "../styles/NotFound.css";
+import { AuthContext } from "../contexts/AuthContext";
+import { useContext } from "react";
 
 const NotFoundPage = () => {
+  const { isAuthenticated } = useContext(AuthContext);
   return (
-    <NavLink className="NotFound" to="/">
-      <h1>404 Page Not Found, click here to go back to the Homepage</h1>
-      <img src={appLogo} />
-    </NavLink>
+    <>
+      {isAuthenticated ? (
+        <>
+          <NavLink className="NotFound" to="/trips">
+            <h1>404 Page Not Found, click here to go back to your Homepage</h1>
+            <img src={appLogo} />
+          </NavLink>
+        </>
+      ) : (
+        <>
+          <NavLink className="NotFound" to="/">
+            <h1>404 Page Not Found, click here to go back to the Homepage</h1>
+            <img src={appLogo} />
+          </NavLink>
+        </>
+      )}
+    </>
   );
 };
 
