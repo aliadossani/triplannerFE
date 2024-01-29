ea07f1ebb6b32314d73f998c31cae7ffe237b95c
Emiliano Foti <foti.emiliano@gmail.com>
2024-01-29 14:07:13 +0100

    conditional rendering on homepage when already logged in

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,32 @@
-const HomePage = () => {
-    return (
-        <>
-        <h1>Welcome to TripPlanner !</h1>
-        <p>Say goodbye to the hassle of planning holidays with friends! Are you tired of spending too much time organizing every detail? Let us handle the boring part, so you can sit back, relax, and enjoy your vacation !</p>
-        </>
-    )}
+import { AuthContext } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
 
-export default HomePage
+const HomePage = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/trips");
+    }
+  }, [isAuthenticated, navigate]);
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <>
+          <h1>Welcome to TripPlanner !</h1>
+          <p>
+            Say goodbye to the hassle of planning holidays with friends! Are you
+            tired of spending too much time organizing every detail? Let us
+            handle the boring part, so you can sit back, relax, and enjoy your
+            vacation !
+          </p>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export default HomePage;
