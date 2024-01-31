0ec5a2e4cf48946c45bf1ab943eeeac66024bc3f
Emiliano Foti <foti.emiliano@gmail.com>
2024-01-31 11:08:19 +0100

    clear the searchbar when item is added

--- src/components/ChangeGrocery.jsx
+++ src/components/ChangeGrocery.jsx
@@ -1,43 +1,69 @@
 import { useState } from "react";
-import { TextInput, Button, Select } from '@mantine/core';
+import { TextInput, Button, Select } from "@mantine/core";
 
-const ChangeGrocery = ({handleAddGrocery,handleEditGrocery, userId, trip, grocery = {}}) => {
-    const {name, quantity, label} = grocery;
-  const [newGrocery, setNewGrocery] = useState({ name: name || '', quantity: quantity || '', label: label || '' });
+const ChangeGrocery = ({
+  handleAddGrocery,
+  handleEditGrocery,
+  userId,
+  trip,
+  grocery = {},
+  setGroceryAdded,
+}) => {
+  const { name, quantity, label } = grocery;
+  const [newGrocery, setNewGrocery] = useState({
+    name: name || "",
+    quantity: quantity || "",
+    label: label || "",
+  });
 
   const handleChange = async (event) => {
     const { name, value } = event.target;
     setNewGrocery((prevGrocery) => ({ ...prevGrocery, [name]: value }));
   };
-    return (
-        <>
-        {userId === trip.createdBy && (
-            <div>
-                <TextInput label="Name" name="name" defaultValue={newGrocery.name} onChange={handleChange} />
-                <TextInput label="Quantity" name="quantity" defaultValue={newGrocery.quantity} onChange={handleChange} />
-                <Select
-                    label="Label"
-                    name="label"
-                    placeholder="Select a label"
-                    defaultValue={newGrocery.label}
-                    data={['Someone will bring it', 'Needs to be purchased']}
-                    onChange={(event) => setNewGrocery((prevGrocery) => ({ ...prevGrocery, "label": event }))}
-                />
-                <Button mt="md" fullWidth onClick={(event) => {
-                    if(grocery?.name) {
-                        handleEditGrocery(event, newGrocery)
-                    } else {
-                        handleAddGrocery(event, newGrocery)
-                    }
-                    }}>
-                    {
-                        grocery?.name ? "Edit Grocery" : "Add Grocery"
-                    }
-                </Button>
-            </div>
-        )}
-        </>
-    )
-}
+  return (
+    <>
+      {userId === trip.createdBy && (
+        <div>
+          <TextInput
+            label="Name"
+            name="name"
+            defaultValue={newGrocery.name}
+            onChange={handleChange}
+          />
+          <TextInput
+            label="Quantity"
+            name="quantity"
+            defaultValue={newGrocery.quantity}
+            onChange={handleChange}
+          />
+          <Select
+            label="Label"
+            name="label"
+            placeholder="Select a label"
+            defaultValue={newGrocery.label}
+            data={["Someone will bring it", "Needs to be purchased"]}
+            onChange={(event) =>
+              setNewGrocery((prevGrocery) => ({ ...prevGrocery, label: event }))
+            }
+          />
+          <Button
+            mt="md"
+            fullWidth
+            onClick={(event) => {
+              if (grocery?.name) {
+                handleEditGrocery(event, newGrocery);
+              } else {
+                handleAddGrocery(event, newGrocery);
+                setGroceryAdded(true);
+              }
+            }}
+          >
+            {grocery?.name ? "Edit Grocery" : "Add Grocery"}
+          </Button>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default ChangeGrocery;
--- src/pages/TripDetailsPage.jsx
+++ src/pages/TripDetailsPage.jsx
@@ -1,20 +1,21 @@
-import { useParams, useNavigate } from 'react-router-dom'
-import { useContext, useEffect, useState } from 'react'
-import { IconEdit, IconTrash } from '@tabler/icons-react';
-import { Modal, Button } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import { AuthContext } from '../contexts/AuthContext'
+import { useParams, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { IconEdit, IconTrash } from "@tabler/icons-react";
+import { Modal, Button } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { AuthContext } from "../contexts/AuthContext";
 import classes from "../styles/TripDetailsPage.module.css";
-import GroceryList from '../components/GroceryList';
-import ChangeGrocery from '../components/ChangeGrocery';
-import ParticipantList from '../components/ParticipantList';
+import GroceryList from "../components/GroceryList";
+import ChangeGrocery from "../components/ChangeGrocery";
+import ParticipantList from "../components/ParticipantList";
 
 const TripDetailsPage = () => {
-  const { tripId } = useParams()
+  const { tripId } = useParams();
   const [trip, setTrip] = useState();
   const [grocery, setGrocery] = useState({});
   const [opened, { open, close }] = useDisclosure(false);
-  const { fetchWithToken, userId } = useContext(AuthContext)
+  const { fetchWithToken, userId } = useContext(AuthContext);
+  const [groceryAdded, setGroceryAdded] = useState(false);
   const navigate = useNavigate();
 
   const fetchTrip = async () => {
@@ -27,7 +28,7 @@ const TripDetailsPage = () => {
         setTrip(tripData);
       } else {
         alert("Couldn't fetch Trips");
-        console.log('Something went wrong')
+        console.log("Something went wrong");
       }
     } catch (error) {
       console.log(error);
@@ -53,11 +54,11 @@ const TripDetailsPage = () => {
       } else {
         const errorData = await response.json();
         alert("Couldn't add grocery item. Reason: " + errorData.message);
-        console.log('Failed to add grocery item:', errorData);
+        console.log("Failed to add grocery item:", errorData);
       }
     } catch (error) {
-        alert("Couldn't add grocery item. Reason: " + error.message);
-        console.log('Error adding grocery item:', error);
+      alert("Couldn't add grocery item. Reason: " + error.message);
+      console.log("Error adding grocery item:", error);
     }
   };
 
@@ -72,92 +73,113 @@ const TripDetailsPage = () => {
       } else {
         const errorData = await response.json();
         alert("Couldn't delete grocery item. Reason: " + errorData.message);
-        console.log('Failed to delete grocery item:', errorData);
+        console.log("Failed to delete grocery item:", errorData);
       }
     } catch (error) {
-        alert("Couldn't delete grocery item" + error.message);
-        console.log('Error delete grocery item. Reason: ', error);
+      alert("Couldn't delete grocery item" + error.message);
+      console.log("Error delete grocery item. Reason: ", error);
     }
   };
 
   const handleEditGroceryModal = async (grocery) => {
     setGrocery(grocery);
     open();
-  }
+  };
 
   const handleEditGrocery = async (event, currentGrocery) => {
     event.preventDefault();
     try {
-        const payload = {
-          ...currentGrocery,
-        };
-        const response = await fetchWithToken(`/groceryitems/${grocery._id}`, "PUT", payload);
-        if (response.ok) {
-          await fetchTrip();
-          close();
-        } else {
-          const errorData = await response.json();
-          alert("Couldn't add grocery item. Reason: " + errorData.message);
-          console.log('Failed to add grocery item:', errorData);
-        }
-      } catch (error) {
-          alert("Couldn't add grocery item. Reason: " + error.message);
-          console.log('Error adding grocery item:', error);
+      const payload = {
+        ...currentGrocery,
+      };
+      const response = await fetchWithToken(
+        `/groceryitems/${grocery._id}`,
+        "PUT",
+        payload
+      );
+      if (response.ok) {
+        await fetchTrip();
+        close();
+      } else {
+        const errorData = await response.json();
+        alert("Couldn't add grocery item. Reason: " + errorData.message);
+        console.log("Failed to add grocery item:", errorData);
       }
+    } catch (error) {
+      alert("Couldn't add grocery item. Reason: " + error.message);
+      console.log("Error adding grocery item:", error);
+    }
+  };
 
-  }
-
-  const handleDeleteTrip = async(event) => {
+  const handleDeleteTrip = async (event) => {
     event.preventDefault();
     try {
-        const response = await fetchWithToken(
-          `/trips/${tripId}`,
-          "DELETE"
-        );
-        if (response.ok) {
-          navigate("/trips");
-        } else {
-          const errorData = await response.json();
-          alert("Couldn't delete trips. Reason: " + errorData.message);
-          console.log('Failed to delete trips:', errorData);
-        }
-      } catch (error) {
-          alert("Couldn't delete trips" + error.message);
-          console.log('Error delete trips. Reason: ', error);
+      const response = await fetchWithToken(`/trips/${tripId}`, "DELETE");
+      if (response.ok) {
+        navigate("/trips");
+      } else {
+        const errorData = await response.json();
+        alert("Couldn't delete trips. Reason: " + errorData.message);
+        console.log("Failed to delete trips:", errorData);
       }
-  }
+    } catch (error) {
+      alert("Couldn't delete trips" + error.message);
+      console.log("Error delete trips. Reason: ", error);
+    }
+  };
 
   return trip ? (
     <>
-        <div className={classes.headerContainer}>
-            <div className={classes.tripImageContainer}>
-                <img src={trip.image} className={classes.tripImage}/>
-            </div>
-            <div className={classes.headerContent}>
-                <h3>{trip.title}</h3>
-                <h4>{trip.description}</h4>
-                <h4>{trip.destination}</h4>
-            </div>
-            <div>
-                <IconEdit className={classes.ctaBtn} onClick={() => navigate(`/trips/${tripId}/update`)}  />
-                <IconTrash className={classes.ctaBtn} onClick={handleDeleteTrip}  />
-            </div>
+      <div className={classes.headerContainer}>
+        <div className={classes.tripImageContainer}>
+          <img src={trip.image} className={classes.tripImage} />
         </div>
-        <ParticipantList trip={trip} />
-        <GroceryList trip={trip} handleDeleteGrocery={handleDeleteGrocery} handleEditGroceryModal={handleEditGroceryModal} />
-        
-        <Modal
-            opened={opened}
-            title="Grocery Item"
-            centered
-            onClose={() => {
-                close();
-                setGrocery({})
-            }}>
-            <ChangeGrocery trip={trip} handleAddGrocery={handleAddGrocery} handleEditGrocery={handleEditGrocery} userId={userId} grocery={grocery} /> 
-        </Modal>
-        <Button mr="sm" onClick={open}>Add grocery</Button>
-        <Button onClick={() => navigate(`/trips/${tripId}/shoppinglist`)}>Print the shopping list</Button>
+        <div className={classes.headerContent}>
+          <h3>{trip.title}</h3>
+          <h4>{trip.description}</h4>
+          <h4>{trip.destination}</h4>
+        </div>
+        <div>
+          <IconEdit
+            className={classes.ctaBtn}
+            onClick={() => navigate(`/trips/${tripId}/update`)}
+          />
+          <IconTrash className={classes.ctaBtn} onClick={handleDeleteTrip} />
+        </div>
+      </div>
+      <ParticipantList trip={trip} />
+      <GroceryList
+        handleDeleteGrocery={handleDeleteGrocery}
+        handleEditGroceryModal={handleEditGroceryModal}
+        handleAddGrocery={handleAddGrocery}
+        groceryAdded={groceryAdded}
+        setGroceryAdded={setGroceryAdded}
+      />
+
+      <Modal
+        opened={opened}
+        title="Grocery Item"
+        centered
+        onClose={() => {
+          close();
+          setGrocery({});
+        }}
+      >
+        <ChangeGrocery
+          trip={trip}
+          handleAddGrocery={handleAddGrocery}
+          handleEditGrocery={handleEditGrocery}
+          userId={userId}
+          grocery={grocery}
+          setGroceryAdded={setGroceryAdded}
+        />
+      </Modal>
+      <Button mr="sm" onClick={open}>
+        Add grocery
+      </Button>
+      <Button onClick={() => navigate(`/trips/${tripId}/shoppinglist`)}>
+        Print the shopping list
+      </Button>
     </>
   ) : (
     <h2>Loading...</h2>
