add20a0014a1cc86adddc444ecd8c26de2907988
Alia Dossani <dossanialia1408@gmail.com>
2024-02-01 16:28:09 +0100

    added the routes to Error Page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -7,9 +7,11 @@ import { Button, Container } from "@mantine/core";
 import errorImage from "../assets/404Page.jpg";
 
 const NotFoundPage = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+  const redirectionURL = isAuthenticated ? "/trips" : "/";
   return (
     <Container size="sm">
-      <NavLink className="NotFound" to="/trips">
+      <NavLink className="NotFound" to={redirectionURL}>
         <img src={errorImage} />
         <Button>Back to Home Page</Button>
       </NavLink>
